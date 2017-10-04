71e9e9bcc26db7b4856e5138e2e30c1e428fa23b
songxh0424 <songxh@umich.edu>
2017-10-04 16:21:31 -0400

    updated outputs and script

--- lab5/crawl_comments.py
+++ lab5/crawl_comments.py
@@ -1,49 +1,8 @@
-import urllib3
-import json
-from bs4 import BeautifulSoup
-import codecs
-import datetime
-
-# reader = codecs.getreader("utf-8")
-# app_id = '176975276185964'
-# app_secret = '39c0a736cfd732bc153e87bb0d1b92e1'
-
-# # url = 'https://graph.facebook.com/v2.10/wsj?fields=posts%%7Bcomments%%7D&access_token=%s|%s' % (app_id, app_secret)
-# getid_url = 'https://graph.facebook.com/v2.10/wsj?fields=id&access_token=%s|%s' % (app_id, app_secret)
-# req = requests.get(getid_url)
-# data = req.json()
-
-# url = 'https://graph.facebook.com/v2.10/%s/comments?filter=stream&access_token=%s|%s' % (data['id'], app_id, app_secret)
-# url = 'https://graph.facebook.com/v2.10/nytimes?fields=posts{comments}&filter=stream&access_token=%s|%s' % (app_id, app_secret)
-# req = requests.get(url)
-# raw = req.json()
-
-# http = urllib3.PoolManager()
-# req = http.request('GET', url)
-
-# data = req.data
-# data = data.decode('utf-16')
-# data_js = json.loads(data)
-
-# test_url = 'https://graph.facebook.com/v2.10/wsj?fields=id,name&access_token=%s|%s' % (app_id, app_secret)
-
-# req = http.request('GET', test_url)
-# data = req.data
-# data_json = json.loads(data.decode('utf-8'))
-
-# import requests
-
-# raw = json.loads(req.text)
-
-
-################################################################################
-## new
-################################################################################
 import requests
 import json
-import datetime
+import re
 
-url_posts = 'https://graph.facebook.com/v2.10/nytimes/posts?since=20 september 2017&until=27 september 2017&access_token=%s|%s' % (app_id, app_secret)
+url_posts = 'https://graph.facebook.com/v2.10/nytimes/posts?since=20 september 2017&until=27 september 2017&limit=100&access_token=%s|%s' % (app_id, app_secret)
 
 req = requests.get(url_posts)
 data = req.json()
@@ -63,6 +22,27 @@ with open('comments.txt', 'w+', encoding = 'utf-8') as f:
     for c in comments:
         f.write(c + '\n')
 
+## ignores non-ascii characters, but also loses some spanish characters
 # with open('comments-bytes.txt', 'wb+') as f:
 #     for c in comments:
-#         f.write((c + '\n').encode('utf-8'))
+#         f.write((c + '\n').encode('ascii', 'ignore'))
+
+## experimenting emoji removal
+myre = re.compile(u'['
+    u'\U0001F300-\U0001F64F'
+    u'\U0001F680-\U0001F6FF'
+    u'\U0001F910-\U0001F9EF'
+    u'\U0001F1E0-\U0001F1FF'
+    u'\U0000203C'
+    # u'\U000000A9-\U0000269C'
+    u'\U000026A0-\U0001F18E'
+    # u'\U0001F190-\U0001F251'
+    u'\u2600-\u26FF\u2700-\u27BF]+', 
+    re.UNICODE)
+
+with open('comments-noemo.txt', 'w+', encoding = 'utf-8') as f:
+    for c in comments:
+        f.write(myre.sub('', c + '\n'))
+
+## comments without any emoji
+comments_emoless = [myre.sub('', c + '\n') for c in comments]
