04d4c491441320ab671666f2d227f36f9d97685d
songxh0424 <songxh@umich.edu>
2017-10-05 00:42:23 -0400

    uploaded random sample of comments; renamed files; updated html template

--- lab5/si618_f17_lab5_songxh_jihzhong.py
+++ lab5/si618_f17_lab5_songxh_jihzhong.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import re
+import random
+import csv
 
 url_posts = 'https://graph.facebook.com/v2.10/nytimes/posts?since=20 september 2017&until=27 september 2017&limit=100&access_token=%s|%s' % (app_id, app_secret)
 
@@ -10,22 +12,17 @@ data = req.json()
 ## get post ids
 ids = [dic['id'] for dic in data['data']]
 ## get all comments under each post
+post_ids = []
+comment_ids = []
 comments = []
 for i in ids:
     url = 'https://graph.facebook.com/v2.10/%s/comments?filter=stream&limit=3000&access_token=%s|%s' % \
         (i, app_id, app_secret)
     req = requests.get(url)
     data_com = req.json()['data']
+    post_ids += [i for dic in data_com]
+    comment_ids += [dic['id'] for dic in data_com]
     comments += [dic['message'] for dic in data_com]
-
-with open('comments.txt', 'w+', encoding = 'utf-8') as f:
-    for c in comments:
-        f.write(c + '\n')
-
-## ignores non-ascii characters, but also loses some spanish characters
-# with open('comments-bytes.txt', 'wb+') as f:
-#     for c in comments:
-#         f.write((c + '\n').encode('ascii', 'ignore'))
 
 ## experimenting emoji removal
 myre = re.compile(u'['
@@ -40,9 +37,19 @@ myre = re.compile(u'['
     u'\u2600-\u26FF\u2700-\u27BF]+', 
     re.UNICODE)
 
-with open('comments-noemo.txt', 'w+', encoding = 'utf-8') as f:
-    for c in comments:
-        f.write(myre.sub('', c + '\n'))
-
 ## comments without any emoji
-comments_emoless = [myre.sub('', c + '\n') for c in comments]
+comments_emoless = [myre.sub('', c).replace('\n', ' ') for c in comments]
+## remove empty comments
+cmnt_lst = list(zip(post_ids, comment_ids, comments_emoless))
+for i in range(len(cmnt_lst)-1, -1, -1):
+    if cmnt_lst[i][2].strip() == '':
+        cmnt_lst.pop(i)
+
+cmnt_kept = random.sample(cmnt_lst, 100)
+
+with open('si618_f17_lab5_random_sample_100_comments_songxh_jhzhong.csv', 'w+', encoding = 'utf-8') as f:
+    w = csv.writer(f)
+    w.writerow(['pagename','post_id','comment_id','comment'])
+    for c in cmnt_kept:
+        w.writerow(['nytimes', *c])
+
